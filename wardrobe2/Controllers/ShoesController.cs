110bef01d70f185f57d428e250bf74115979625d
kingveee <vivianpham22@gmail.com>
2017-08-05 17:56:04 -0400

    first commit

---
+++ wardrobe2/Controllers/ShoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using wardrobe2.Models;
+
+namespace wardrobe2.Controllers
+{
+    public class ShoesController : Controller
+    {
+        private WardrodeEntities db = new WardrodeEntities();
+
+        // GET: Shoes
+        public ActionResult Index()
+        {
+            return View(db.Shoes.ToList());
+        }
+
+        // GET: Shoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sho sho = db.Shoes.Find(id);
+            if (sho == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sho);
+        }
+
+        // GET: Shoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ShoeID,ShoeName,ShoeType,ShoeColor,ShoeSeason,ShoeOccasion,ShoePhoto")] Sho sho)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Shoes.Add(sho);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sho);
+        }
+
+        // GET: Shoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sho sho = db.Shoes.Find(id);
+            if (sho == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sho);
+        }
+
+        // POST: Shoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ShoeID,ShoeName,ShoeType,ShoeColor,ShoeSeason,ShoeOccasion,ShoePhoto")] Sho sho)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sho).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sho);
+        }
+
+        // GET: Shoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sho sho = db.Shoes.Find(id);
+            if (sho == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sho);
+        }
+
+        // POST: Shoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sho sho = db.Shoes.Find(id);
+            db.Shoes.Remove(sho);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
